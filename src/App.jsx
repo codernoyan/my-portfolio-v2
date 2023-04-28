2b93bbab97f3c12530a7bad35eff29a87e1900b2
codernoyan <coder.noyan@gmail.com>
2023-04-28 23:14:18 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Home from "pages/Home"
+
 function App() {
   return (
-    <div>
-      <h2 className="text-xl">hello</h2>
-    </div>
+    <>
+      <Home />
+    </>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,125 @@
+import { useState } from "react";
+
 export default function Navbar() {
+  const [isToggleOpen, setIsToggleOpen] = useState(false);
+
+  // navbar menus
+  const menus = <>
+    <li role="none" className="flex items-stretch">
+      <a
+        role="menuitem"
+        aria-haspopup="false"
+        tabIndex="0"
+        className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-cyan-500 focus:outline-none focus-visible:outline-none lg:px-4"
+        href="/#about"
+      >
+        <span>About</span>
+      </a>
+    </li>
+    <li role="none" className="flex items-stretch">
+      <a
+        role="menuitem"
+        aria-current="page"
+        aria-haspopup="false"
+        tabIndex="0"
+        className="flex items-center gap-2 py-4 text-cyan-500 transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-4"
+        href="/#skills"
+      >
+        <span>Skills</span>
+      </a>
+    </li>
+    <li role="none" className="flex items-stretch">
+      <a
+        role="menuitem"
+        aria-haspopup="false"
+        tabIndex="0"
+        className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-cyan-500 focus:outline-none focus-visible:outline-none lg:px-4"
+        href="/#works"
+      >
+        <span>Works</span>
+      </a>
+    </li>
+    <li role="none" className="flex items-stretch">
+      <a
+        role="menuitem"
+        aria-haspopup="false"
+        tabIndex="0"
+        className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-cyan-500 focus:outline-none focus-visible:outline-none lg:px-4"
+        href="/#contact"
+      >
+        <span>Contact</span>
+      </a>
+    </li>
+    <li role="none" className="flex items-stretch">
+      <div className="me-auto flex items-center md:px-6 lg:ml-0 lg:p-0">
+        <button className="border-2 border-cyan-500 px-4 py-1 rounded-sm hover:bg-cyan-500 hover:text-white transition-colors">
+          <span>Hire Me</span>
+        </button>
+      </div>
+    </li>
+  </>
+
   return (
-    <section>
-      
-    </section>
+    <>
+      {/*<!-- Component: Navbar with CTA --> */}
+      <header className="relative z-20 w-full bg-white/90 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full lg:backdrop-blur-sm lg:after:hidden">
+        <div className="relative max-w-full md:px-0">
+          <nav
+            aria-label="main navigation"
+            className="flex h-16 items-stretch justify-between font-medium text-slate-700"
+            role="navigation"
+          >
+            {/*      <!-- Brand logo --> */}
+            <div
+              id="WindUI"
+              aria-label="WindUI logo"
+              aria-current="page"
+              className="flex items-center gap-2 whitespace-nowrap py-3 text-lg focus:outline-none lg:flex-1"
+            >
+              <a href="#">noyandey</a>
+            </div>
+            {/*      <!-- Mobile trigger --> */}
+            <button
+              className={`relative order-10 block h-10 w-10 self-center lg:hidden
+              ${isToggleOpen
+                  ? "visible opacity-100 [&_span:nth-child(1)]:w-6 [&_span:nth-child(1)]:translate-y-0 [&_span:nth-child(1)]:rotate-45 [&_span:nth-child(3)]:w-0 [&_span:nth-child(2)]:-rotate-45 "
+                  : ""
+                }
+            `}
+              onClick={() => setIsToggleOpen(!isToggleOpen)}
+              aria-expanded={isToggleOpen ? "true" : "false"}
+              aria-label="Toggle navigation"
+            >
+              <div className="absolute top-1/2 left-1/2 w-6 -translate-x-1/2 -translate-y-1/2 transform">
+                <span
+                  aria-hidden="true"
+                  className="absolute block h-0.5 w-9/12 -translate-y-2 transform rounded-full bg-slate-900 transition-all duration-300"
+                ></span>
+                <span
+                  aria-hidden="true"
+                  className="absolute block h-0.5 w-6 transform rounded-full bg-slate-900 transition duration-300"
+                ></span>
+                <span
+                  aria-hidden="true"
+                  className="absolute block h-0.5 w-1/2 origin-top-left translate-y-2 transform rounded-full bg-slate-900 transition-all duration-300"
+                ></span>
+              </div>
+            </button>
+            {/*      <!-- Navigation links --> */}
+            <ul
+              role="menubar"
+              aria-label="Select page"
+              className={`absolute top-0 left-0 z-[-1] h-[28.5rem] w-full justify-center overflow-hidden  overflow-y-auto overscroll-contain bg-white/90 pr-8 pb-12 pt-16 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0 lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0  lg:pt-0 lg:opacity-100 ${isToggleOpen
+                ? "visible opacity-100 backdrop-blur-sm"
+                : "invisible opacity-0"
+                }`}
+            >
+              {menus}
+            </ul>
+          </nav>
+        </div>
+      </header>
+      {/*<!-- End Navbar with CTA --> */}
+    </>
   )
 }
