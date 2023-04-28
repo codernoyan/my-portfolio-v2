4626317a5df66db7f9d72d3da6fb4ef583e10404
codernoyan <coder.noyan@gmail.com>
2023-04-28 23:18:09 +0600

    padding added to parent div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "components/navbar/Navbar";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-2 md:px-0">
       <Navbar />
     </div>
   )
