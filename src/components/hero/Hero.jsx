18c18a27acb4b6aef469beac2e7df456277eaf1f
codernoyan <coder.noyan@gmail.com>
2023-04-29 11:36:52 +0600

    blurhash image added to hero image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,13 +1,41 @@
 import authorImage from 'assets/images/Noayn_Dey.jpg';
+import { useEffect, useState } from 'react';
+import { BlurhashCanvas } from "react-blurhash";
 
 export default function Hero() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  useEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setImageLoaded(true);
+    };
+    img.src = authorImage;
+  }, [])
   return (
     <section className="flex justify-center mt-4">
       <div>
         {/* image */}
         <div>
-          <div className="bg-cyan-200 rounded-full p-2">
-            <img src={authorImage} alt="author" className="h-32 rounded-full" loading="lazy" />
+          <div className="bg-cyan-200 rounded-full p-2 flex">
+            {/* <img src={authorImage} alt="author" className="h-32 rounded-full" loading="lazy" /> */}
+            <>
+              {
+                !imageLoaded && (
+                  <BlurhashCanvas
+                    hash="LfPZlsWY_Na#i[ocxuoeXUWWVsWD"
+                    resolutionX={32}
+                    resolutionY={32}
+                    punch={1}
+                    className="rounded-full"
+                  />
+                )
+              }
+              {
+                imageLoaded && (
+                  <img src={authorImage} alt="author" className="h-32 rounded-full" loading="lazy" />
+                )
+              }
+            </>
           </div>
         </div>
         {/* designation */}
