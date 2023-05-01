f896f1ed528e6f8e9df2babf4973c62461386b47
codernoyan <coder.noyan@gmail.com>
2023-05-01 11:54:58 +0600

    about section created

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,13 +2,13 @@ import noyanImage from 'assets/images/Noayn_Dey.jpg';
 
 export default function About() {
   return (
-    <section className="h-screen mt-8">
+    <section className="my-8">
       <div>
-        <h2 className="text-2xl uppercase font-bold text-center">About</h2>
+        <h2 className="text-2xl uppercase font-bold text-center mb-8">About</h2>
       </div>
-      <div className="flex flex-col md:flex-row gap-4 items-center">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 justify-items-center">
         {/* image */}
-        <div className="md:w-full">
+        <div className="md:w-auto lg:w-full">
           <div className="group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500">
             <span className="absolute inset-0 translate-x-1 translate-y-1 bg-indigo-600 transition-transform group-hover:translate-y-0 group-hover:translate-x-0" />
             <span className="relative block border border-current bg-white">
@@ -17,11 +17,16 @@ export default function About() {
           </div>
         </div>
         {/* description */}
-        <div>
+        <div className="col-span-3 flex flex-col gap-4 lg:gap-0 justify-evenly items-center lg:items-baseline">
           <p className="text-center md:text-justify font-medium text-slate-600">
-            I am Noyan. Love to write code with a passion. From my childhood, I had a dream to become a web developer. Today that dream has been fulfilled through hard work, dedication, and sacrifice. I am self-motivated and skilled in web technologies. I am an expert in building applications with the JavaScript UI library React, Node.js, Express.js, and CRUD operations with MongoDB Database.I am passionate about programming, especially building web applications. When I write code, motivation comes from within myself, which helps me to complete a task perfectly.
+            I am Noyan. Love to write code with a passion. From my childhood, I had a dream to become a web developer. Today that dream has been fulfilled through hard work, dedication, and sacrifice. I am self-motivated and skilled in web technologies. I am an expert in building applications with the JavaScript UI library React, Redux, Node.js, Express.js, and CRUD operations with MongoDB Database.I am passionate about programming, especially building web applications. When I write code, motivation comes from within myself, which helps me to complete a task perfectly.
           </p>
-          <a href="/">Download Resume</a>
+          <a href="/" download="Noyan_Dey.pdf" className="group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500">
+            <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-y-0 group-hover:translate-x-0" />
+            <span className="relative block border border-current bg-white font-bold px-8 py-3">
+              Download Resume
+            </span>
+          </a>
         </div>
       </div>
     </section>
