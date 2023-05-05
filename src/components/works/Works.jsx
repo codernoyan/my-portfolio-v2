9f6f512ba3571ea7f2a0f924cd1e7f4212a45a12
codernoyan <coder.noyan@gmail.com>
2023-05-05 19:25:21 +0600

    blur image added to works cards

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -15,9 +15,9 @@ export default function Works() {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {/* card 1 */}
-        <div className="w-full bg-slate-50 p-2 rounded-md">
+        <div className="w-full bg-slate-50 p-2 rounded-md cursor-pointer">
           {/* image */}
-          <div>
+          <div className="relative font-medium text-indigo-600 before:absolute before:rounded-sm before:aspect-video before:w-full before:origin-left before:scale-x-0 before:backdrop-blur-lg hover:before:scale-100 duration-500 transition-all before:duration-700 before:transition-transform before:ease-in-out before:bg-black/25">
             <img src={projectOne} alt="projectOne" className="aspect-video object-cover" />
           </div>
           {/* text content */}
@@ -27,9 +27,9 @@ export default function Works() {
           </div>
         </div>
         {/* card 2 */}
-        <div className="w-full bg-slate-50 p-2 rounded-md">
+        <div className="w-full bg-slate-50 p-2 rounded-md cursor-pointer">
           {/* image */}
-          <div>
+          <div className="relative font-medium text-indigo-600 before:absolute before:rounded-sm before:aspect-video before:w-full before:origin-left before:scale-x-0 before:backdrop-blur-lg hover:before:scale-100 before:duration-700 before:transition-transform before:ease-in-out before:bg-black/25">
             <img src={projectTwo} alt="projectTwo" className="aspect-video w-full object-cover" />
           </div>
           {/* text content */}
@@ -39,9 +39,9 @@ export default function Works() {
           </div>
         </div>
         {/* card 3 */}
-        <div className="w-full bg-slate-50 p-2 rounded-md">
+        <div className="w-full bg-slate-50 p-2 rounded-md cursor-pointer">
           {/* image */}
-          <div>
+          <div className="relative font-medium text-indigo-600 before:absolute before:rounded-sm before:aspect-video before:w-full before:origin-left before:scale-x-0 before:backdrop-blur-lg hover:before:scale-100 before:duration-700 before:transition-transform before:ease-in-out before:bg-black/25">
             <img src={projectThree} alt="projectThree" className="aspect-video object-cover" />
           </div>
           {/* text content */}
@@ -51,10 +51,16 @@ export default function Works() {
           </div>
         </div>
       </div>
-      <div className="flex">
+      {/* <div className="flex">
         <div className="h-40 w-60 object-cover bg-black" ></div>
         <div className="h-40 w-60 object-cover bg-indigo-100" ></div>
-      </div>
+      </div> */}
+      <a className="relative font-medium text-indigo-600 before:absolute before:-bottom-1 before:h-48 before:w-48 before:origin-left before:scale-x-0 before:backdrop-blur-md before:transition hover:before:scale-100" href="/download">
+        <img src={projectThree} alt="projectThree" className="h-48 w-48 object-cover" />
+        <div className="absolute top-9 hidden">
+          <h2>hello</h2>
+        </div>
+      </a>
     </section>
   )
 }
