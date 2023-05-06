883c17c67bad748fe92942de619b96dec216b509
codernoyan <coder.noyan@gmail.com>
2023-05-06 18:26:38 +0600

    white space bugs fixed on about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,7 +18,7 @@ export default function About() {
           </div>
         </div>
         {/* description */}
-        <div className="col-span-3 flex flex-col gap-4 lg:gap-0 justify-evenly items-center lg:items-baseline">
+        <div className="lg:col-span-3 flex flex-col gap-4 lg:gap-0 justify-evenly items-center lg:items-baseline">
           <p className="text-center md:text-justify font-medium text-slate-600">
             I am Noyan. Love to write code with a passion. From my childhood, I had a dream to become a web developer. Today that dream has been fulfilled through hard work, dedication, and sacrifice. I am self-motivated and skilled in web technologies. I am an expert in building applications with the JavaScript UI library React, Redux, Node.js, Express.js, and CRUD operations with MongoDB Database.I am passionate about programming, especially building web applications. When I write code, motivation comes from within myself, which helps me to complete a task perfectly.
           </p>
