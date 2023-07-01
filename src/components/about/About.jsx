428b5a4c625341e2f8fd30c44b6499d4230c9dad
codernoyan <coder.noyan@gmail.com>
2023-07-02 01:51:13 +0600

    about page updated with new description

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -20,12 +20,12 @@ export default function About() {
         {/* description */}
         <div className="lg:col-span-3 flex flex-col gap-4 lg:gap-0 justify-evenly items-center lg:items-baseline">
           <h1 className="text-xl font-bold">Noyan Dey</h1>
-          <p className="bg-indigo-50 p-1 rounded-md text-indigo-600 font-medium">Frontend Web Developer</p>
+          <p className="bg-indigo-50 p-1 rounded-md text-indigo-600 font-medium">MERN Stack Developer</p>
           <p className="text-center md:text-justify font-medium text-slate-600">
-            I am Noyan. Love to write code with a passion. From my childhood, I had a dream to become a web developer. Today that dream has been fulfilled through hard work, dedication, and sacrifice. I am self-motivated and skilled in web technologies. I am an expert in building applications with the JavaScript UI library React, Redux, Node.js, Express.js, and CRUD operations with MongoDB Database.I am passionate about programming, especially building web applications. When I write code, motivation comes from within myself, which helps me to complete a task perfectly.
+            I am Noyan. I have an unwavering passion for writing code that fuels my creativity and ignites my soul. I have always been curious and passionate about technology since my childhood. Initially, I started with graphic design but jumped over the web development train to fulfill my dream. I prioritize user-centric design, focusing on intuitive and seamless experiences. Collaboration and continuous learning are integral to my work, as I thrive in dynamic environments and stay updated with the latest trends. Check out my portfolio to see my projects and let's build something amazing together.
           </p>
           {/* resume download button */}
-          <a href="https://drive.google.com/file/d/1VdirqCSEyo2K5rjkQU2JPeNC03EWWbBW/view?usp=share_link" download="Noyan_Dey.pdf" target="_blank" rel="noreferrer" className="group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500">
+          <a href="https://drive.google.com/file/d/1cv10ivFfWX_6W3qBOSkhaTYZ7A07h1vE/view?usp=drive_link" download="Noyan_Dey.pdf" target="_blank" rel="noreferrer" className="group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500">
             <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-y-0 group-hover:translate-x-0" />
             <span className="relative block border border-current bg-white font-bold px-8 py-3">
               Download Resume
